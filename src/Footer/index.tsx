86e775c6ad60d400ce48be573ea5bad643f45abe
Denis Souza <denis.souza@ulteriorti.com.br>
2022-08-12 20:36:54 -0300

    Footer adicionado

---
+++ src/Footer/index.tsx
@@ -0,0 +1,14 @@
+import "./styles.css"
+import { ReactComponent as Logo } from "./logo.svg"
+function Footer() {
+    return (
+       <footer className="main-footer">
+        <div className="footer-icons">
+            
+        </div>
+            <p>App de entregas open-source</p>
+       </footer>
+
+    )
+}
+export default Footer
