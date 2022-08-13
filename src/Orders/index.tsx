328ac1638535e0e1cdc0aa76e61f78b85d8d9fdd
Denis Souza <denis.souza@ulteriorti.com.br>
2022-08-12 22:10:39 -0300

    StepsHeader

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,8 +1,9 @@
+import StepsHeader from './StepsHeader'
 import './styles.css'
 function Orders() {
     return (
-        <div>
-            <h1>Orders</h1>
+        <div className='orders-container'>
+            <StepsHeader />
         </div>
     )
 }
