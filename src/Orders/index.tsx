509b9af45cebebe3c59b15fd9980913ee0e4c3af
Denis Souza <denis.souza@ulteriorti.com.br>
2022-08-13 16:23:40 -0300

    Obtenção de produtos da api

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react'
+import { fetchProducts } from '../api'
+import ProductList from './ProductList'
 import StepsHeader from './StepsHeader'
 import './styles.css'
+import { Product } from './Types'
 function Orders() {
+    
+    const [products, setProducts] = useState<Product[]>([])
+    
+    useEffect(()=>{
+        fetchProducts().then(
+            response => setProducts(response.data)
+        ).catch(
+            error => console.log(error)
+        )
+    },[])
     return (
         <div className='orders-container'>
             <StepsHeader />
+            <ProductList products={products}/>
         </div>
     )
 }
